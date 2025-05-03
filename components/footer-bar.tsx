70e74fc5622e801ac8035de5789ad4013babdf35
icebear0111 <wltjq0901@naver.com>
2025-05-03 11:50:22 +0900

    feat(main-page): Footer 추가 [#14]
    푸터 컴포넌트로 분리하고 메인페이지에 추가
    - 페이지 통일성을 위해 푸터 추가

---
+++ components/footer-bar.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="border-gray-800 text-center text-gray-400">
+        <p>© 2025 페이득. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
