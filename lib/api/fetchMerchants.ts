5b5bfc07e6752a450c29c7add6e5b614dd97869b
sungjoon <dkdldrha@naver.com>
2025-05-19 10:57:51 +0900

    feat(api) : 가맹점 목록 조회 API [#84] - page : 가맹점 목록 및 통계를 조회하고 컴포넌트 렌더링 - fetchMerchants.ts, fetchMerchantStatistics.ts => api 호출 파일

---
+++ lib/api/fetchMerchants.ts
@@ -0,0 +1,67 @@
+// api/fetchMerchants.ts
+
+import { fetchWithAuth } from "@/lib/api-fetch";
+
+export interface MerchantApiResponse {
+  id: number;
+  merchantName: string;
+  category: string;
+  transactionCount: number;
+  transactionAmount: number;
+  status: boolean;
+}
+
+export interface Merchant {
+  id: number;
+  name: string;
+  category: string;
+  transactions: number;
+  amount: number;
+  status: "활성" | "비활성";
+}
+export interface Pageable {
+  page: number;
+  size: number;
+}
+export interface PaginatedMerchantData {
+  content: Merchant[];
+  totalPages: number;
+  totalElements: number;
+  pageable: Pageable;
+}
+
+export const fetchMerchants = async (
+  page = 1,
+  size = 5,
+): Promise<PaginatedMerchantData> => {
+  const response = await fetchWithAuth(
+    `/admin/merchants?page=${page}&size=${size}`,
+  );
+  const data = await response.json();
+  if (!data.success) {
+    // 에러처리해야됌
+    return {
+      content: [],
+      totalPages: 0,
+      totalElements: 0,
+      pageable: { page: page, size: size },
+    };
+  }
+
+  const content: Merchant[] = data.response.content.map(
+    (m: MerchantApiResponse) => ({
+      id: m.id,
+      name: m.merchantName,
+      category: m.category,
+      transactions: m.transactionCount,
+      amount: m.transactionAmount,
+      status: m.status ? "활성" : "비활성",
+    }),
+  );
+  return {
+    content,
+    totalPages: data.response.totalPages,
+    totalElements: data.response.totalElements,
+    pageable: data.response.pageable,
+  };
+};
