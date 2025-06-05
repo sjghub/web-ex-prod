9b03daaa4dc5acfe438fb33e27c166c17c10e6d9
Lim Jiseob <wltjq0901@naver.com>
2025-06-05 11:18:36 +0900

    Feat/#127 파비콘 추가 및 public 폴더 정리 (#128)
    * feat(favicon): 파비콘 추가 [#127]
    
    * chore(public): public 폴더 정리 [#127]

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "페이득",
   description: "카드 추천 간편 결제 시스템",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
